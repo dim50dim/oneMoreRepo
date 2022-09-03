98d9468e998a0d936afb93578af568e4a49e1dc1
dim50dim <zheliezkov@gmail.com>
2022-09-03 18:43:02 +0300

    ///

--- src/script.js
+++ src/script.js
@@ -4,6 +4,9 @@ const t10 = (num,letter) => num + letter;
 function sayHi() {
     console.log('Hello from Git');
 }
+//////////////////
+//////////////////
+//////////////////
 function work (){
   console.log('WORK');
 }
